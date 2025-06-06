e626179d8d5b57818ced157fb6d48bad668d2ab0
零式的天空 <246009756@qq.com>
2025-06-06 15:51:32 +0800

    Update Config.java

--- app/src/main/java/cn/moon/ql/Config.java
+++ app/src/main/java/cn/moon/ql/Config.java
@@ -6,7 +6,7 @@ public class Config {
 
 //
 //    // 青龙后台地址，请修改
-    public static final String QL_URL = "https://ql.email-routing.us";
+    public static final String QL_URL = "https://jdapp.010085.xyz";
 //
 //    // 接口信息，请修改
     public static String QL_CLIENT_ID = "UG9KQg-xJv0N";
